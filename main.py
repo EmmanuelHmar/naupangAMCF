e188786a8006ccc8a26510707e2958a1faaf58d0
Emmanuel Hmar <ehmar@pgprint.com>
2019-03-15 17:05:43 -0400

    added much texts

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
+import sqlite3 as lite
+import sys
 import random
+
+con = lite.connect('test.db')
+
+cursor = con.cursor()
+
+print("Opened db success")
+
+cursor.execute('''CREATE TABLE INKHAWM 
+    (ID INT PRIMARY KEY NOT NULL,
+    NAME TEXT NOT NULL,
+    DEPARTMENT TEXT NOT NULL);''')
+
+cursor.close()
+
+'''
+try:
+    con = lite.connect('test.db')
+    ur = con.cursor() 
+    cur.execute('SELECT SQLITE_VERSION()')
+    data = cur.fetchone()
+    print("Error %s:" % e.args[0])
+    pass
+except: #expression as identifier: #lite.Error as e:
+    print("Error %s:" % e.args[0])
+    sys.exit(1)
+finally:
+    if con:
+        con.close() '''
 
 juniorDept = ['Lalrinsanga','Andrew Rampana','Juliana Vanlalhlimpuii','Melody Malsawmhlui ']
 
@@ -8,10 +38,18 @@ beginnerDept = ['Jayson Lalrammawia','Joel Vanlalrinmuana','Esther Lalrinliani',
 
 zirtirtu = ['Pi Machhuani','Lalremruati','Lalrawngbawla']
 
-output = '1. Chairperson : Melody Malsawmhlui  (Junior Dept) \n2. Inkhawm hlan tawngtaitu : Caleb Lalrinliana (Primary Dept) \n3. Bible chhiartu : Lalrinsanga (Junior Dept) 4. Thilpek khawntu :Joel Vanlalrinmuana (Beginner Dept)'
+dept = ['Pi Machhuani[Junior]','Lalremruati[Primary]','Lalrawngbawla[Junior']
 
- #5. Thawhlawm hlan tawngtaitu: Lalremruati
 
-# 6. Solo - Andrew Rampana (Junior Dept) 7. Attendance - Hming lamna 8. Lal biakna sawi rual'
+chairperson = '1. Chairperson : ' #Junior Dept
+inkhawmHlan = '2. Inkhawm hlan tawngtaitu : '
+bibleChhiar = '3. Bible chhiartu : ' #Junior Dept
+thilPekKhawntu = '4. Thilpek khawntu : Joel Vanlalrinmuana (Beginner Dept)' #Beginner Dept
+thawhHlawmHlantu = '5. Thawhlawm hlan tawngtaitu: ' #Zirtirtu
+solo = '6. Solo - '
+attendance = '7. Attendance - Hming lamna'
+lalBiakSawi = '8. Lal biakna sawi rual'
 
-print(output)
+print(chairperson + random.choice(juniorDept))
+print(inkhawmHlan + random.choice(primaryDept))
+print(bibleChhiar + random.choice(beginnerDept))
