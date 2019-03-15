e7af7815e4a9ab274ed92071812e1e552fb5df63
EmmanuelHmar <eman.hmar@gmail.com>
2019-03-15 19:39:11 -0400

    added db py setup

---
+++ dbSetup.py
@@ -0,0 +1,35 @@
+import sqlite3 as lite
+import sys
+import random
+
+con = lite.connect('db.db')
+
+cursor = con.cursor()
+
+print("Opened db success")
+
+cursor.execute('''CREATE TABLE NAUPANG 
+    (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    NAME TEXT NOT NULL,
+    DEPARTMENT TEXT NOT NULL);''')
+
+personSql = "INSERT INTO NAUPANG (NAME,DEPARTMENT) VALUES (?,?)"
+cursor.execute(personSql,('Andrew Rampana','Junior Dept'))
+cursor.execute(personSql,('Juliana Vanlalhlimpuii','Junior Dept'))
+
+zirtirtuQuery = '''CREATE TABLE ZIRTIRTU 
+    (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    NAME TEXT NOT NULL,
+    DEPARTMENT TEXT NOT NULL,
+    COUNT INTEGER NOT NULL DEFAULT 0);'''
+
+cursor.execute(zirtirtuQuery)
+
+insertZirtirtu = "INSERT INTO ZIRTIRTU (NAME,DEPARTMENT) VALUES (?,?)"
+cursor.execute(insertZirtirtu,('Lalrawngbawla','Junior Dept'))
+cursor.execute(insertZirtirtu,('Pi Machhuani','Beginner Dept'))
+cursor.execute(insertZirtirtu,('Lalremruati','Primary Dept'))
+
+
+con.commit()
+con.close()
--- main.py
+++ main.py
@@ -2,33 +2,11 @@ import sqlite3 as lite
 import sys
 import random
 
-con = lite.connect('test.db')
+con = lite.connect('db.db')
 
 cursor = con.cursor()
 
-print("Opened db success")
-
-cursor.execute('''CREATE TABLE INKHAWM 
-    (ID INT PRIMARY KEY NOT NULL,
-    NAME TEXT NOT NULL,
-    DEPARTMENT TEXT NOT NULL);''')
-
-cursor.close()
-
-'''
-try:
-    con = lite.connect('test.db')
-    ur = con.cursor() 
-    cur.execute('SELECT SQLITE_VERSION()')
-    data = cur.fetchone()
-    print("Error %s:" % e.args[0])
-    pass
-except: #expression as identifier: #lite.Error as e:
-    print("Error %s:" % e.args[0])
-    sys.exit(1)
-finally:
-    if con:
-        con.close() '''
+print("Opened db from Main")
 
 juniorDept = ['Lalrinsanga','Andrew Rampana','Juliana Vanlalhlimpuii','Melody Malsawmhlui ']
 
@@ -43,7 +21,7 @@ dept = ['Pi Machhuani[Junior]','Lalremruati[Primary]','Lalrawngbawla[Junior']
 
 chairperson = '1. Chairperson : ' #Junior Dept
 inkhawmHlan = '2. Inkhawm hlan tawngtaitu : '
-bibleChhiar = '3. Bible chhiartu : ' #Junior Dept
+bibleChhiar = '3. Bible chhiartu : ' #Junior Dept #Primary Dept
 thilPekKhawntu = '4. Thilpek khawntu : Joel Vanlalrinmuana (Beginner Dept)' #Beginner Dept
 thawhHlawmHlantu = '5. Thawhlawm hlan tawngtaitu: ' #Zirtirtu
 solo = '6. Solo - '
