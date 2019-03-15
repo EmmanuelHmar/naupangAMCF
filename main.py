13a9a56cd34566f9db14527462648c7c9b4688a1
EmmanuelHmar <eman.hmar@gmail.com>
2019-03-15 06:22:49 -0400

    output files

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ beginnerDept = ['Jayson Lalrammawia','Joel Vanlalrinmuana','Esther Lalrinliani',
 
 zirtirtu = ['Pi Machhuani','Lalremruati','Lalrawngbawla']
 
+output = '1. Chairperson : Melody Malsawmhlui  (Junior Dept) \n2. Inkhawm hlan tawngtaitu : Caleb Lalrinliana (Primary Dept) \n3. Bible chhiartu : Lalrinsanga (Junior Dept) 4. Thilpek khawntu :Joel Vanlalrinmuana (Beginner Dept)'
+
+ #5. Thawhlawm hlan tawngtaitu: Lalremruati
+
+# 6. Solo - Andrew Rampana (Junior Dept) 7. Attendance - Hming lamna 8. Lal biakna sawi rual'
+
+print(output)
