b96cf3ed9fc89f62f57658eb539a7ac817932020
EmmanuelHmar <eman.hmar@gmail.com>
2019-03-15 06:15:28 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+juniorDept = ['Lalrinsanga','Andrew Rampana','Juliana Vanlalhlimpuii','Melody Malsawmhlui ']
+
+primaryDept = ['Elizabeth Lalawmpuii','Caleb Lalrinliana','Joseph Rinmawia']
+
+beginnerDept = ['Jayson Lalrammawia','Joel Vanlalrinmuana','Esther Lalrinliani','Kristy Rinmawii']
+
+zirtirtu = ['Pi Machhuani','Lalremruati','Lalrawngbawla']
+
